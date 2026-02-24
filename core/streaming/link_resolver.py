46724334e87bd76f196a4744f0a166c084df37bf
cassildobez2-star <cassildobez2@gmail.com>
2026-02-24 08:15:47 +0200

    Delete core/streaming directory

--- core/streaming/link_resolver.py
+++
@@ -1,9 +0,0 @@
-from streaming.drive_resolver import resolve_drive
-
-
-async def resolve_link(url: str):
-
-    if "drive.google.com" in url:
-        return await resolve_drive(url)
-
-    return url
